f3ceed5d4846ce48cd5b946ee7c18ec632b8c5e1
Arnon Bright <arnonbright@gmail.com>
2025-02-16 15:57:10 +0700

    Added TMDB api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
+
+const API_BASE_URL = "https://api.themoviesdb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  Headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    }
+  };
+
+  useEffect(() => {}, []);
   return (
     <main>
       <div className="pattern" />
@@ -14,9 +37,10 @@ const App = () => {
             Find <span className="text-gradient">Moives</span> You'll Enjoy
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section></section>
       </div>
     </main>
   );
